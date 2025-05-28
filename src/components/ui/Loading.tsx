7e6dcc8fbfdc19203467b113a868563f05a96ca8
Mostafa <mostafa.fahimipour@gmail.com>
2025-05-28 16:07:01 +0330

    feat: add loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <span className="inline-block w-8 h-8 border-t-2 rounded-full border-t-blue-500 animate-spin"></span>
+    </div>
+  );
+}
