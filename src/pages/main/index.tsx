c58ae5f46fe9f162861dfe4cd1f6f82150b1df22
Mostafa <mostafa.fahimipour@gmail.com>
2025-05-28 15:57:06 +0330

    fix: responsive was resolved

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -7,13 +7,13 @@ import { UserColumn } from "~/pages/main/components/user-column/UserColumn";
 export default function MainPage() {
   return (
     <div className="flex flex-wrap h-screen p-5 overflow-auto">
-      <div className="flex items-center w-full sm:w-1/2 md:w-1/3 h-full overflow-auto">
+      <div className="flex items-center w-full md:w-1/2 xl:w-1/3 h-full overflow-auto">
         <UserColumn />
       </div>
-      <div className="flex items-center w-full sm:w-1/2 md:w-1/3  h-full overflow-auto">
+      <div className="flex items-center w-full md:w-1/2 xl:w-1/3 mt-10 md:mt-0  h-full overflow-auto">
         <ProductColumn />
       </div>
-      <div className="flex items-center w-full sm:w-1/2 md:w-1/3 h-full overflow-auto">
+      <div className="flex items-center w-full md:w-1/2  xl:w-1/3 mt-10 xl:mt-0 h-full overflow-auto">
         <CartColumn />
       </div>
     </div>
